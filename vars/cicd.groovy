801f276cd19fcec78e7d9aad026170530ae80a45
Ashwani0102 <163709752+Ashwani0102@users.noreply.github.com>
2024-03-18 12:24:54 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,4 +10,8 @@ def newMaven()
 //{
   //sh "deploy adapters: [tomcat9"credentialsId: '2d7bffd1-b890-4532-a303-e2f1dcf6d44a', path: '', url: ${ip}"], contextPath: ${contextPath}, war: '**/*.war'"
 //}
+def newTest(jobname)
+{
+  sh 'java -jar /home/ubuntu/.jenkins/workspace/${jobname}/testing.jar'
+}
 
