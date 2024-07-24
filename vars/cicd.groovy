3c3a94183a6e3c6c5fbb81d4f40f9074eaa717d6
Ashwani0102 <163709752+Ashwani0102@users.noreply.github.com>
2024-07-24 16:12:20 +0530

    d

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -13,7 +13,7 @@ def newDeploy(jobName,IP,context)
   sh "scp /home/ubuntu/.jenkins/workspace/${jobName}/webapp/target/webapp.war ubuntu@${IP}:/opt/tomcat9/webapps/${context}.war"
 }
 
-def seleniumTesting(jobname)
+def seleniumTesting(jobName)
 {
   sh "java -jar /home/ubuntu/.jenkins/workspace/${jobName}/testing.jar"
 }
