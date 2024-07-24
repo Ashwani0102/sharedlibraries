c773346cfa5a59fadc6de5ef65be39e171a041f9
Ashwani0102 <163709752+Ashwani0102@users.noreply.github.com>
2024-07-24 15:35:08 +0530

    c

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -7,3 +7,9 @@ def newMaven()
 {
   sh 'mvn package'
 }
+
+def newDeploy(jobName,IP,contextpath)
+{
+  sh 'scp /home/ubuntu/.jenkins/workspace/{jobName}/webapp/target/webapp.war ubuntu@{IP}:/opt/tomcat9/webapps/${contextPath}.war'
+}
+
